1662a1ff4ea4d0da1c4e031f055a2b4085395947
Riyaj K M <riyajkafar@gmail.com>
2023-05-21 02:06:40 +0530

    role management

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -19,8 +20,21 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * Register any authentication / authorization services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        $this->registerPolicies();
+
+        Gate::before(function ($user, $ability) {
+            if ($user->role === 'super_admin') {
+                return true; // Super admin has all permissions
+            }
+        });
+
+        Gate::define('manage_users', function ($user) {
+            return in_array($user->role, ['super_admin', 'admin']);
+        });
+
+        // Define other permissions and gate definitions as needed
+
     }
 }
